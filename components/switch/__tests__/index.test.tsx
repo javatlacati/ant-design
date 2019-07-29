ae6a4b175549b5b28948ac252c158216dee1a390
Javatlacati <scherzo_16@hotmail.com>
2019-07-28 21:23:12 -0500

    migrated switch component test to typescript

--- components/switch/__tests__/index.test.tsx
+++ components/switch/__tests__/index.test.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Switch from '..';
+// @ts-ignore
 import focusTest from '../../../tests/shared/focusTest';
 
 describe('Switch', () => {
   focusTest(Switch);
 
   it('should has click wave effect', async () => {
-    const wrapper = mount(<Switch />);
-    wrapper
+    const wrapper = mount<Switch>(<Switch />);
+    const domNode: any = wrapper
       .find('.ant-switch')
-      .getDOMNode()
+      .getDOMNode();
+    domNode
       .click();
     await new Promise(resolve => setTimeout(resolve, 0));
     expect(wrapper.render()).toMatchSnapshot();
