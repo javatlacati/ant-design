5b2e68bb91dd92f8e4e8cc1ce2181973aa8f0918
Javatlacati <scherzo_16@hotmail.com>
2019-07-28 21:35:35 -0500

    migrated mention component test to typescript

--- components/mention/__tests__/index.test.tsx
+++ components/mention/__tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { mount } from 'enzyme';
 import Mention from '..';
 
@@ -15,23 +15,27 @@ describe('Mention', () => {
 
   it('should has focus function', () => {
     const handleFocus = jest.fn();
+    const suggestions: Array<React.ReactElement> = [React.createElement('p', null, 'afc163'), React.createElement('p', null, 'benjycui'), React.createElement('p', null, 'yiminghe'), React.createElement('p', null, 'RaoHai'), React.createElement('p', null, '中文'), React.createElement('p', null, 'にほんご')];
     const wrapper = mount(
       <Mention
         defaultValue={toContentState('@afc163')}
         onFocus={handleFocus}
-        suggestions={['afc163', 'benjycui', 'yiminghe', 'RaoHai', '中文', 'にほんご']}
+        suggestions={suggestions}
       />,
     );
-    wrapper.instance().focus();
+    wrapper.prop('onFocus')();
     jest.runAllTimers();
     expect(handleFocus).toHaveBeenCalled();
   });
 
   it('basic suggestion', () => {
     const handleSearch = jest.fn();
+
+    const suggestions: Array<React.ReactElement> = [React.createElement('p', null, 'afc163'), React.createElement('p', null, 'raohai')];
     const wrapper = mount(
-      <Mention suggestions={['afc163', 'raohai']} onSearchChange={handleSearch} />,
+      <Mention suggestions={suggestions} onSearchChange={handleSearch} />,
     );
+    wrapper.setProps({ suggestions: ['afc163', 'raohai'] });
     wrapper.find('DraftEditorContents').simulate('focus');
     const ed = wrapper.find('.public-DraftEditor-content');
     ed.simulate('beforeInput', { data: '@a' });
@@ -41,12 +45,15 @@ describe('Mention', () => {
 
   it('change suggestions', () => {
     const container = mount(<div id="container" />);
+
+    const suggestions = [React.createElement('', null, 'afc163'), React.createElement('', null, 'raohai')];
     const wrapper = mount(
       <Mention
-        suggestions={['afc163', 'raohai']}
+        suggestions={suggestions}
         getSuggestionContainer={() => container.getDOMNode()}
       />,
     );
+    wrapper.setProps({ suggestions: ['afc163', 'raohai'] });
     wrapper.find('DraftEditorContents').simulate('focus');
     const ed = wrapper.find('.public-DraftEditor-content');
     ed.simulate('beforeInput', { data: '@' });
@@ -66,9 +73,12 @@ describe('Mention', () => {
   it('select item', () => {
     const onChange = jest.fn();
     const onSelect = jest.fn();
+    const array: Array<React.ReactElement> = [React.createElement('p', null, 'afc163'), React.createElement('p', null, 'raohai')];
+
     const wrapper = mount(
-      <Mention suggestions={['afc163', 'raohai']} onChange={onChange} onSelect={onSelect} />,
+      <Mention suggestions={array} onChange={onChange} onSelect={onSelect} />,
     );
+    wrapper.setProps({ suggestions: ['afc163', 'raohai'] });
     wrapper.find('DraftEditorContents').simulate('focus');
     const ed = wrapper.find('.public-DraftEditor-content');
     ed.simulate('beforeInput', { data: '@' });
@@ -118,7 +128,8 @@ describe('Mention', () => {
     if (process.env.REACT === '15') {
       return;
     }
-    const wrapper = mount(<Mention defaultSuggestions={[<Mention.Nav value="light" />]} />);
+    const defaultSuggestions = [<Mention.Nav key={123} value="light" />];
+    const wrapper = mount(<Mention defaultSuggestions={defaultSuggestions} />);
     wrapper.find('DraftEditorContents').simulate('focus');
     const ed = wrapper.find('.public-DraftEditor-content');
     ed.simulate('beforeInput', { data: '@l' });
