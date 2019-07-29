568039d6c76bd023afa2f5c231670230afedab66
Javatlacati <scherzo_16@hotmail.com>
2019-07-28 21:59:58 -0500

    migrated component tests to typescript

--- components/__tests__/util/domHook.ts
+++ components/__tests__/util/domHook.ts
@@ -1,16 +1,16 @@
-export function spyElementPrototypes(Element, properties) {
+export function spyElementPrototypes(Element: any, properties: any) {
   const propNames = Object.keys(properties);
-  const originDescriptors = {};
+  const originDescriptors: any = {};
 
   propNames.forEach(propName => {
-    const originDescriptor = Object.getOwnPropertyDescriptor(Element.prototype, propName);
+    const originDescriptor: PropertyDescriptor = Object.getOwnPropertyDescriptor(Element.prototype, propName)!;
     originDescriptors[propName] = originDescriptor;
 
     const spyProp = properties[propName];
 
     if (typeof spyProp === 'function') {
       // If is a function
-      Element.prototype[propName] = function spyFunc(...args) {
+      Element.prototype[propName] = function spyFunc(...args: any) {
         return spyProp.call(this, originDescriptor, ...args);
       };
     } else {
@@ -21,12 +21,14 @@ export function spyElementPrototypes(Element, properties) {
           if (spyProp.set) {
             return spyProp.set.call(this, originDescriptor, value);
           }
+          // @ts-ignore
           return originDescriptor.set(value);
         },
         get() {
           if (spyProp.get) {
             return spyProp.get.call(this, originDescriptor);
           }
+          // @ts-ignore
           return originDescriptor.get();
         },
       });
@@ -35,7 +37,7 @@ export function spyElementPrototypes(Element, properties) {
 
   return {
     mockRestore() {
-      propNames.forEach(propName => {
+      propNames.forEach((propName: string) => {
         const originDescriptor = originDescriptors[propName];
         if (typeof originDescriptor === 'function') {
           Element.prototype[propName] = originDescriptor;
@@ -47,7 +49,7 @@ export function spyElementPrototypes(Element, properties) {
   };
 }
 
-export function spyElementPrototype(Element, propName, property) {
+export function spyElementPrototype(Element: any, propName: string, property: any) {
   return spyElementPrototypes(Element, {
     [propName]: property,
   });
